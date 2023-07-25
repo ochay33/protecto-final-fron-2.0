44e37afcf9079581a2cdf65922d2e5d392457c02
PabloD16 <pablodaniel2244@gmail.com>
2023-07-24 23:05:28 -0300

    footer

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../../css/footer.css";
+
+export function Footer() {
+  return (
+    <div className="footer-container">
+      <p> &copy; 2021 Mila Nadir. Todos los derechos reservados</p>
+    </div>
+  );
+}
